7af7cfdac8f1ac4af125d35a55ded716e4a0c967
ToppleKek <topkek@topplekek.xyz>
2024-11-04 19:41:19 -0500

    Attempt to debug tilemap resizing

--- editor.cpp
+++ editor.cpp
@@ -9,10 +9,12 @@ static void resize_tilemap(u16 new_width, u16 new_height) {
     ICHIGO_INFO("New tilemap size: %ux%u (%u)", new_width, new_height, new_width * new_height);
     u32 width_delta = DISTANCE(Ichigo::Internal::current_tilemap.width, new_width) * sizeof(u16);
     auto *p = &Ichigo::Internal::current_tilemap.tiles[Ichigo::Internal::current_tilemap.width];
+    ICHIGO_INFO("init Should be 2: %u Should be 3: %u", *p, *(p - 1));
     for (u32 i = 1; i < Ichigo::Internal::current_tilemap.height; ++i) {
         std::memmove(p + width_delta, p, (Ichigo::Internal::current_tilemap.height - i) * Ichigo::Internal::current_tilemap.width * sizeof(u16));
         std::memset(p, 0, width_delta);
         p += width_delta + Ichigo::Internal::current_tilemap.width * sizeof(u16);
+        ICHIGO_INFO("i=%u Should be 2: %u Should be 3: %u", i, *p, *(p - 1));
     }
 
     Ichigo::Internal::current_tilemap.width  = new_width;
